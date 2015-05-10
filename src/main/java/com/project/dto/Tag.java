7b2a13c4332e70d7c63e2544de7d57630e1d9a09
waykar-prashant <waykar.prashant@gmail.com>
2015-05-09 21:14:44 -0700

    Tag

--- src/main/java/com/project/dto/Tag.java
+++ src/main/java/com/project/dto/Tag.java
@@ -6,6 +6,27 @@ public class Tag {
 	//tag count
 	private int count;
 	//tag date
+	private String date;
+	
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public int getCount() {
+		return count;
+	}
+	public void setCount(int count) {
+		this.count = count;
+	}
+	public String getDate() {
+		return date;
+	}
+	public void setDate(String date) {
+		this.date = date;
+	}
+	
 	
 	
 
